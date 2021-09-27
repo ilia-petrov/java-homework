2e5436200e8400a5b6302eb43ef3fbb8a53e1f08
ilia-petrov <66006789+ilia-petrov@users.noreply.github.com>
2021-09-27 17:37:28 +0300

    Add files via upload

--- Tema6/Problem3.java
+++ Tema6/Problem3.java
@@ -1,21 +1,18 @@
 import java.util.Scanner;
 
 public class Problem3 {
+
+
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        int n;
-        double sum = 0;
 
-        System.out.println("Enter n:");
-        n = sc.nextInt();
+        Rectangular f = new Rectangular(new Point(sc.nextInt(), sc.nextInt()),
+                                        new Point(sc.nextInt(), sc.nextInt())),
+                    s = new Rectangular(new Point(sc.nextInt(), sc.nextInt()),
+                                        new Point(sc.nextInt(), sc.nextInt()));
 
-        while(n > 0)
-        {
-            sum += (1.0 / n);
-            n --;
-        }
 
-        System.out.println(sum);
+        System.out.println(f.isPointIn(s.u) && f.isPointIn(s.d));
     }
 }
